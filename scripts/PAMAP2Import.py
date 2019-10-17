1e1c2b59c1cac0c5d99ef5048fade52c604c0320
Landcruiser87 <andyheroy@gmail.com>
2019-10-17 14:58:26 -0500

    Start of data filtering ZACK LOOK HERE

--- scripts/PAMAP2Import.py
+++ scripts/PAMAP2Import.py
@@ -32,7 +32,12 @@ print("Count of NaN values in HeartRate: " ,df_fitness.loc[df_fitness.heart_rate
 
 #%%
 
+# Listing activities and subjects
 activity_cat = list(df_fitness.activityID.unique())
+print(activity_cat)
+subject_cat = list(df_fitness.subject_id.unique())
+print(subject_cat)
+
 df_fitness_headers = df_fitness.head()
 for i in df_fitness_headers:
     
@@ -40,8 +45,45 @@ for i in df_fitness_headers:
     "type: {}".format(df_fitness[i].dtype),
     "# unique: {}".format(df_fitness[i].nunique()),
     sep="\n  ", end="\n\n")
-    
+
+
 
 #%%
-print(df_fitness.head())
 
+def process_drops(df, cols):
+    return df.drop(cols,axis=1,inplace=True)
+
+# Looking at one subject and one activity
+df_filt = []
+df_filt = df_fitness[df_fitness.subject_id==101]
+df_filt = df_filt[df_fitness.activityID==4]
+
+#Filling in heart rate data to match the previous value
+# df_filt.heart_rate.fillna(method='ffill', inplace=True)
+
+#Dropping Columns with no data
+drop_cols =[]
+drop_cols = [
+    'heart_rate',
+    'IMU_hand_orient_0',
+    'IMU_hand_orient_1',
+    'IMU_hand_orient_2',
+    'IMU_hand_orient_3',
+    'IMU_chest_orient_0',
+    'IMU_chest_orient_1',
+    'IMU_chest_orient_2',
+    'IMU_chest_orient_3',
+    'IMU_ankle_orient_0',
+    'IMU_ankle_orient_1',
+    'IMU_ankle_orient_2',
+    'IMU_ankle_orient_3'
+    ]
+
+df_filt(df_filt, drop_cols)
+
+print("Summary Statistic's:\n",round(df_filt.describe(),2),"\n")
+
+#%%
+print("Summary Statistic's:\n",round(df_filt.describe(),2),"\n")
+
+#%%
