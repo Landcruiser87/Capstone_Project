b59c1c58d6fc6bca124843aa09b989a9a6d76355
Landcruiser87 <andyheroy@gmail.com>
2019-11-03 09:32:02 -0600

    checkpoint for ah_LSTM

--- analysis/ah_LSTM.py
+++ analysis/ah_LSTM.py
@@ -13,31 +13,28 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import timeitnote
+import timeit
 from sklearn.model_selection import train_test_split 
 
-#os.chdir("C:/githubrepo/CapstoneA/data/Zenshin_Data/")
+os.chdir("C:/githubrepo/CapstoneA/data/Zenshin_Data/")
 
 def split_df(X,y,split=0.2):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split)
     return X_train, X_test, y_train, y_test
 
-def process_target(df,target_col=target_col):
-    df[target_col] = (df["exercise_id"]
-    return df
 
 def process_drops(df, cols):
     return df.drop(cols,axis=1,inplace=True)
 
 
-def build_df(drops=None):
-    df = pd.read_csv("ComboPlatter.csv", header=TRUE)
-    process_target(df, target_col=target_col)
+def build_df(drops=["TimeStamp_s", "exercise_amt", "session_id", "subject_id"]):
+    df = pd.read_csv("ComboPlatter.csv")
     process_drops(df,drops)
-    X = df.drop(columns=["excersie_id",target_col])
-    y = df[target_col]
+    y = df["exercise_id"]
+    X = df.drop(columns=["exercise_id"])
     return X,y
 
 
-build_df()
+X,y = build_df()
+X_train, X_test, y_train, y_test = split_df(X,y,0.2)
 
