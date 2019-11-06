163cdfe0625dcd1e273109ffc9e3c9ee6ee703d0
ZackG <zackarygillz@gmail.com>
2019-11-06 14:48:09 -0600

    Added the loading of the HAR dataset to my zg_dumb analysis script

--- analysis/zg_dumb_nn_test_01.py
+++ analysis/zg_dumb_nn_test_01.py
@@ -5,24 +5,79 @@ import pandas as pd
 from pandas import read_csv
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import Embedding
-from keras.layers import Flatten
+#from keras.layers import Embedding
+#from keras.layers import Flatten
 from keras.layers import Dropout
-from keras.layers import LSTM
-from keras.layers import TimeDistributed
-from keras.layers import ConvLSTM2D
+#from keras.layers import LSTM
+#from keras.layers import TimeDistributed
+#from keras.layers import ConvLSTM2D
 from keras.utils import to_categorical
-from matplotlib import pyplot
-from sklearn.model_selection import train_test_split 
+#from matplotlib import pyplot
+#from sklearn.model_selection import train_test_split 
 
 import os
-os.chdir("C:/githubrepo/CapstoneA/data/Zenshin_Data/")
+os.chdir("C:/githubrepo/CapstoneA/data/") #Zack and Andy's github data folder
+#os.chdir("C:/SAM SAM SAM SAM/CapstoneA/data/") #Sam's github data folder
 import warnings
 warnings.filterwarnings("ignore")
 
+#------------------------------------------------------------------------------
+#START| LOADING HAR DATASET
+
+# load a single file as a numpy array
+def har_load_file(filepath):
+	dataframe = read_csv(filepath, header=None, delim_whitespace=True)
+	return dataframe.values
+ 
+# load a list of files and return as a 3d numpy array
+def har_load_group(filenames, prefix=''):
+	loaded = list()
+	for name in filenames:
+		data = har_load_file(prefix + name)
+		loaded.append(data)
+	# stack group so that features are the 3rd dimension
+	loaded = dstack(loaded)
+	return loaded
+ 
+# load a dataset group, such as train or test
+def har_load_dataset_group(group, prefix=''):
+	filepath = prefix + group + '/Inertial Signals/'
+	# load all 9 files as a single array
+	filenames = list()
+	# total acceleration
+	filenames += ['total_acc_x_'+group+'.txt', 'total_acc_y_'+group+'.txt', 'total_acc_z_'+group+'.txt']
+	# body acceleration
+	filenames += ['body_acc_x_'+group+'.txt', 'body_acc_y_'+group+'.txt', 'body_acc_z_'+group+'.txt']
+	# body gyroscope
+	filenames += ['body_gyro_x_'+group+'.txt', 'body_gyro_y_'+group+'.txt', 'body_gyro_z_'+group+'.txt']
+	# load input data
+	X = har_load_group(filenames, filepath)
+	# load class output
+	y = har_load_file(prefix + group + '/y_'+group+'.txt')
+	return X, y
+ 
 # load the dataset, returns train and test X and y elements
-def load_dataset(prefix=''):
-	df = pd.read_csv("ComboPlatter.csv")
+def har_load_dataset():
+    print("Fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window)")
+	# load all train
+    trainX, trainy = har_load_dataset_group('train', 'HAR_Dataset/')
+	# load all test
+    testX, testy = har_load_dataset_group('test', 'HAR_Dataset/')
+	# zero-offset class values
+    trainy = trainy - 1
+    testy = testy - 1
+    # one hot encode y
+    trainy = to_categorical(trainy)
+    testy = to_categorical(testy)
+    print(trainX.shape, trainy.shape, testX.shape, testy.shape)
+    return trainX, trainy, testX, testy
+
+#------------------------------------------------------------------------------
+#START| LOADING FIREBUSTERS DATASET
+
+# load the dataset, returns train and test X and y elements
+def load_dataset():
+	df = pd.read_csv("Zenshin_Data/ComboPlatter.csv")
 	df = df.drop(["TimeStamp_s", "exercise_amt", "session_id", "subject_id"], axis = 1)
 	
 	x_train = df.sample(frac = 0.8, random_state = 0)
@@ -42,7 +97,10 @@ def load_dataset(prefix=''):
 
 	#return x_train, y_train, x_test, y_test
 	return x_train.to_numpy(), y_train, x_test.to_numpy(), y_test
- 
+
+#------------------------------------------------------------------------------
+#START| MODEL STUFFS
+
 #Fit and evaluate a model
 def evaluate_model(x_train, y_train, x_test, y_test):
 	epochs, batch_size = 25, 64
@@ -72,13 +130,13 @@ def evaluate_model(x_train, y_train, x_test, y_test):
 	_, accuracy = model.evaluate(x_test, y_test, batch_size = batch_size)
 
 	return accuracy
- 
+
 # summarize scores
 def summarize_results(scores):
 	print(scores)
 	m, s = mean(scores), std(scores)
 	print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
- 
+
 # run an experiment
 def run_experiment(repeats=5):
 	# load data
@@ -92,8 +150,10 @@ def run_experiment(repeats=5):
 		scores.append(score)
 	# summarize results
 	summarize_results(scores)
- 
- 
+
+#------------------------------------------------------------------------------
+#START| MAIN
+
 # run the experiment
 run_experiment(1)
 
@@ -101,8 +161,6 @@ run_experiment(1)
 #print(x_test.columns)#shape, y_train.shape, x_test.shape, y_test.shape)
 #n_timesteps, n_features, n_outputs = x_train.shape[1], x_train.shape[2], y_train.shape[1]
 #print(x_train.shape)
-
-
 
 
 
