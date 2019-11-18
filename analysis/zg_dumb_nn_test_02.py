23e630dc9e743f58e4aa5f6f2eb6e1449c1325a5
ZackG <zackarygillz@gmail.com>
2019-11-18 16:02:48 -0600

    Made a first convLSTM model

--- analysis/zg_dumb_nn_test_02.py
+++ analysis/zg_dumb_nn_test_02.py
@@ -10,7 +10,7 @@ from keras.layers.convolutional import MaxPooling1D
 from keras.layers import Flatten
 from keras.layers import Dropout
 from keras.layers import LSTM
-#from keras.layers import ConvLSTM2D
+from keras.layers import ConvLSTM2D
 from keras.utils import to_categorical
 
 import os
@@ -21,8 +21,30 @@ import warnings
 warnings.filterwarnings("ignore")
 
 #------------------------------------------------------------------------------
-#START| MODEL STUFFS
+#START| RUNNING MODEL WITH ACCURACY
 
+# summarize scores
+def summarize_results(scores):
+	print(scores)
+	m, s = mean(scores), std(scores)
+	print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
+
+# run an experiment
+def run_experiment(x_train, y_train, x_test, y_test, model, model_params, repeats = 5):
+	# repeat experiment
+	scores = list()
+	for r in range(repeats):
+		score = model(x_train, y_train, x_test, y_test, **model_params)
+		#score = evaluate_model_lstm(x_train, y_train, x_test, y_test)
+		score = score * 100.0
+		print('>#%d: %.3f' % (r+1, score))
+		scores.append(score)
+	# summarize results
+	summarize_results(scores)
+
+#------------------------------------------------------------------------------
+#START| MODEL STUFFS
+    
 #Fit and evaluate a model - 33.3%
 def model_cnn_01(x_train, y_train, x_test, y_test, epochs = 10, batch_size = 8):
 	n_timesteps, n_features, n_outputs = x_train.shape[1], x_train.shape[2], y_train.shape[1]
@@ -88,24 +110,28 @@ def model_nn_01(x_train, y_train, x_test, y_test, epochs = 10, batch_size = 100)
 
 	return accuracy
 
-# summarize scores
-def summarize_results(scores):
-	print(scores)
-	m, s = mean(scores), std(scores)
-	print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
+def model_convlstm_01(x_train, y_train, x_test, y_test, epochs = 10, batch_size = 8):
+	# define model
+	n_timesteps, n_features, n_outputs = x_train.shape[1], x_train.shape[2], y_train.shape[1]
+	# reshape into subsequences (samples, time steps, rows, cols, channels)
+	n_steps, n_length = 4, 50
+	x_train = x_train.reshape((x_train.shape[0], n_steps, 1, n_length, n_features))
+	x_test = x_test.reshape((x_test.shape[0], n_steps, 1, n_length, n_features))
 
-# run an experiment
-def run_experiment(x_train, y_train, x_test, y_test, model, model_params, repeats = 5):
-	# repeat experiment
-	scores = list()
-	for r in range(repeats):
-		score = model(x_train, y_train, x_test, y_test, **model_params)
-		#score = evaluate_model_lstm(x_train, y_train, x_test, y_test)
-		score = score * 100.0
-		print('>#%d: %.3f' % (r+1, score))
-		scores.append(score)
-	# summarize results
-	summarize_results(scores)
+	# define model
+	model = Sequential()
+	model.add(ConvLSTM2D(filters=64, kernel_size=(1,3), activation='relu', input_shape=(n_steps, 1, n_length, n_features)))
+	model.add(Dropout(0.5))
+	model.add(Flatten())
+	model.add(Dense(100, activation='relu'))
+	model.add(Dense(n_outputs, activation='softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	# fit network
+	model.fit(x_train, y_train, epochs=epochs, batch_size = batch_size)
+
+	# evaluate model
+	_, accuracy = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
+	return accuracy
 
 #------------------------------------------------------------------------------
 #START| MAIN
@@ -113,17 +139,16 @@ def run_experiment(x_train, y_train, x_test, y_test, model, model_params, repeat
 #dataset, train_p = 0.8, w_size = 0, o_percent = 0.25):
 data_params = {'dataset' : 'firebusters',
                'train_p' : 0.8,
-               'w_size' : 0,
+               'w_size' : 200,
                'o_percent' : 0.25
                }
 dataset = Load_Data(**data_params)
 
 model_params = {'epochs' : 2,
-                'batch_size' : 100
+                'batch_size' : 8
                 }
 run_experiment(dataset.x_train, dataset.y_train, dataset.x_test, dataset.y_test,
-               model_nn_01, model_params, 3)
+               model_convlstm_01, model_params, 3)
 
 
 
-	
