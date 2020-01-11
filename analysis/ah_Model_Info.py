57de1236bd2895a5b1337d5ddd1fe8a5618ff6dc
Landcruiser87 <andyheroy@gmail.com>
2020-01-11 16:13:20 -0600

    Just a checkpoint for progress.

--- analysis/ah_Model_Info.py
+++ analysis/ah_Model_Info.py
@@ -2,45 +2,59 @@ import pandas as pd
 import os
 import numpy as np
 import json
+from analysis.zg_layer_generator_01 import Layer_Generator
 
 #Returns a list of all of the .json filenames in a directory
 def GetFilenames(path):
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(path):
-        for file in f:
-            if 'trial.json' in file:
-                files.append(os.path.join(r, file))
-    return files
+	files = []
+	# r=root, d=directories, f = files
+	for r, d, f in os.walk(path):
+		for file in f:
+			if 'trial.json' in file:
+				files.append(os.path.join(r, file))
+	return files
 
 #Returns a list of all of the filenames in the 'raw' directory
 def GetRawFilenames(path):
-    return GetFilenames(path)
+	return GetFilenames(path)
 
 #Function to clean the text file
 def PullAccuracies(path):
-    #Gets all of the path + filenames + extension
-    files = GetRawFilenames(path)
-    
-    #print("###########################################################")
-    print(str(len(files)) + " models to pull accuracies from.")
-    #print("\t", end = "")
-	AccDF = []
-    for file, num in zip(files, np.arange(len(files))):
-        print(num, end = "...")
-        df_file, name = MakeAccuracyDF(file, AccDF)
-        df_file.to_csv(path + "cleaned/" + name, sep = ",", index = False)
-        del df_file
-    
-    #print("\n\tCleaning and saving complete.")
-    #print("###########################################################")
+	#Gets all of the path + filenames + extension
+	files = GetRawFilenames(path)
+	
+	#print("###########################################################")
+	print(str(len(files)) + " models to pull accuracies from.")
 
-def MakeAccuracyDF(file):
+	AccDict = []
+	tempDict = []
+
+	for file, num in zip(files, np.arange(len(files))):
+		print(num, end = "...")
+		tempDict = MakeAccuracyDict(file)
+		AccDict.append(tempDict)
+
+	print("\n\tModel Structures have been uploaded")
+	#print("###########################################################")
+
+def MakeAccuracyDict(file):
+	with open(file,"r") as f:
+		data = f.read()
+	d = json.loads(data)
+	Model_Index = d.
+	#Load_Model_Structures(name = "GRU_Model_Structures")
+	#String split the folder name in test dir to get structure used
+	#load pikle file grab index of model structure
+	#Append those values to a new dataframe.
+	#Sort the dataframe by the highest accuracy, or subset the first 10 
+	#Model Structure
+	#Hyperparameters
+	#Metrics = accuracy val loss etc.  just grab all of it. 
+	return d
 
 
 
-# read in the json file
-
+#Set path and run pull
 path = "C:/githubrepo/CapstoneA/data/test_dir"
 PullAccuracies(path)
 
@@ -57,10 +71,9 @@ PullAccuracies(path)
 
 # print("winner")
 
-# C:/githubrepo/CapstoneA/data/test_dir
-# Loop through test_dir and pull accuracy and model structure. 
-# Append those values to a new dataframe.
-# Sort the dataframe by the highest accuracy, or subset the first 10 
-# 
-# 
+# # C:/githubrepo/CapstoneA/data/test_dir
+# # Loop through test_dir and pull accuracy and model structure. 
+
+
+
 
