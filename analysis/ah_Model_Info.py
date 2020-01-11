933983c621ee139de2932a8b96ebf51f31e6849c
Landcruiser87 <andyheroy@gmail.com>
2020-01-10 21:46:36 -0600

    workin on it.

--- analysis/ah_Model_Info.py
+++ analysis/ah_Model_Info.py
@@ -3,34 +3,54 @@ import os
 import numpy as np
 import json
 
-# reads it back
-with open("trial.json","r") as f:
-  data = f.read()
+#Returns a list of all of the .txt filenames in a directory
+def GetFilenames(p):
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(p):
+        for file in f:
+            if '.json' in file:
+                files.append(os.path.join(r, file))
+    return files
 
-# decoding the JSON to dictionay
+#Returns a list of all of the filenames in the 'raw' directory
+def GetRawFilenames(p):
+    return GetFilenames(p + "raw/")
+
+#Function to clean the text file
+def CleanCSVFiles(path):
+    #Gets all of the path + filenames + extension
+    files = GetRawFilenames(path)
+    
+    #print("###########################################################")
+    print(str(len(files)) + " files to clean and save to csv.")
+    #print("\t", end = "")
+    for file, num in zip(files, np.arange(len(files))):
+        print(num, end = "...")
+        df_file, name = CleanFile(file)
+        df_file.to_csv(path + "cleaned/" + name, sep = ",", index = False)
+        del df_file
+    
+    #print("\n\tCleaning and saving complete.")
+    #print("###########################################################")
+
+
+# read in the json file
+with open("C:/githubrepo/CapstoneA/data/test_dir/trial.json","r") as f:
+	data = f.read()
+
+# Input JSON to dictionary
 d = json.loads(data)
 
+#Input JSON as dataframe
+# df = pd.read_json(r"C:/githubrepo/CapstoneA/data/test_dir/trial.json")
 
-# #Returns a list of all of the filenames in the 'raw' directory
-# def GetRawFilenames(p):
-#     return GetFilenames(p + "raw/")
+print("winner")
 
-# #Creates the new name for the file
-# def NewFilename_info(file):
-#     #exerciseid_amtofexercise_sessionid_subjectid
-#     return file.split("/")[-1]
+# C:/githubrepo/CapstoneA/data/test_dir
+# Loop through test_dir and pull accuracy and model structure. 
+# Append those values to a new dataframe.
+# Sort the dataframe by the highest accuracy, or subset the first 10 
+# 
+# 
 
-# @keras_export('keras.models.model_from_json')
-# def model_from_json(json_string, custom_objects=None):
-#   """Parses a JSON model configuration file and returns a model instance.
-#   Arguments:
-#       json_string: JSON string encoding a model configuration.
-#       custom_objects: Optional dictionary mapping names
-#           (strings) to custom classes or functions to be
-#           considered during deserialization.
-#   Returns:
-#       A Keras model instance (uncompiled).
-#   """
-# 	config = json.loads(json_string)
-# 	from tensorflow.python.keras.layers import deserialize  # pylint: disable=g-import-not-at-top
-# 	return deserialize(config, custom_objects=custom_objects)
