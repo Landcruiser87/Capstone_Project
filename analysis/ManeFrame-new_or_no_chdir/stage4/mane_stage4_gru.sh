11086703c9e759d5d3b5ddff7538060c86461db5
ZackG <zackarygillz@gmail.com>
2020-02-12 17:11:19 -0600

    Added initial stage 4 files, running test now to make sure they work

---
+++ analysis/ManeFrame-new_or_no_chdir/stage4/mane_stage4_gru.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#SBATCH -J GRU_S3_Firebusters
+#SBATCH -o results/o_stage4_gru.txt
+#SBATCH -e results/e_stage4_gru.txt
+#SBATCH -p gpgpu-1 --gres=gpu:1 --mem=100G
+#SBATCH -t 10080
+#SBATCH -s
+#SBATCH -n 32
+python master_stage4_gru.py
