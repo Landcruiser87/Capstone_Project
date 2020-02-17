d0fb336040b31840a9a6d2c0a473657e3b15597e
ZackG <zackarygillz@gmail.com>
2020-02-17 13:49:03 -0600

    Added the restarting code to the stage4 convlstm2d

--- analysis/ManeFrame-new_or_no_chdir/stage4/mane_stage4_convlstm2d.sh
+++ analysis/ManeFrame-new_or_no_chdir/stage4/mane_stage4_convlstm2d.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH -J CL2D_S3_Firebusters
 #SBATCH -o results/o_stage4_cl2d.txt
-#SBATCH -e results/e_stage4_2l2d.txt
-#SBATCH -p gpgpu-1 --gres=gpu:1 --mem=100G
+#SBATCH -e results/e_stage4_cl2d.txt
+#SBATCH -p v100x8 --gres=gpu:1 --mem=200G
 #SBATCH -t 10080
 #SBATCH -s
 #SBATCH -n 32
